0b71c5d15fce982667f6f1e6f6fe66dfa998c8a6
Jacob Hansen <jshchansen@gmail.com>
2016-12-29 23:24:09 +0100

    messing around some

--- test app/Program.cs
+++ test app/Program.cs
@@ -1,16 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace test_app
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static bool _talking = true;
+        private static void Main(string[] args)
         {
-
+            Console.Write("<<generic stuff>> ");
+            Console.WriteLine("Hello World");
+            while (_talking)
+            {
+                EnCounterDog();
+            }
+            Console.WriteLine("oh look a fox");
+            var foxi = new Fox();
+            foxi.Talk();
+            Console.ReadKey();
         }
+
+        private static void EnCounterDog()
+        {
+            var fido = new GoldenRetriever();
+            Console.WriteLine("ur dog just come running in say Hi");
+            var userInput = Console.ReadLine();
+
+            if (userInput == "Hi")
+            { 
+                fido.Talk();
+                _talking = false;
+            }
+            else
+            {
+                Console.WriteLine("wrong input\nType any key to try again...");
+                Console.ReadKey();
+                Console.Clear();
+                
+            }
+        }
+    }
+
+    internal abstract class Animal
+    {
+        public virtual void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Talk()
+        {
+            Console.WriteLine("generic animal noise");
+        }
+    }
+
+    internal abstract class Dog : Animal
+    {
+        public override void Talk()
+        {
+            Console.WriteLine("Wuuf");
+        }
+    }
+
+    internal class Fox : Animal
+    {
+        
+    }
+
+    
+
+    internal class GoldenRetriever : Dog
+    {
     }
 }
